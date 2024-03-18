5336361620dbf2b43d251c409ff4ca43e5640fae
Muhammad Ihsan <ihsan@femaledaily.com>
2024-03-18 14:33:32 +0700

    feat: learn decorator include http method

---
+++ src/user/user/user.controller.ts
@@ -0,0 +1,60 @@
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Param,
+  Post,
+  Query,
+  Redirect,
+} from '@nestjs/common';
+
+@Controller('/api/users')
+export class UserController {
+  // * http response decorator Header() HttpCode()
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  saampleRespose(): Record<string, string> {
+    return {
+      data: 'Hello json',
+    };
+  }
+
+  // * http response decorator Redirect()
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
+  // * http request query
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') first_name: string,
+    @Query('last_name') last_name: string,
+  ): string {
+    return `Hello ${first_name} ${last_name}`;
+  }
+
+  // * http request params
+  @Get('/:id')
+  getbyiId(@Param('id') id: string): string {
+    return `GET ${id}`;
+  }
+
+  //* simple data
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'GET';
+  }
+}
